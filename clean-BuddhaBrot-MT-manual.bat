6a1ea31372081a83565da7bfa1001f7663ee2bb4
Johan De Bock <johan.debock@gmail.com>
2015-10-03 20:06:06 +0200

    cleanup

---
+++ clean-BuddhaBrot-MT-manual.bat
@@ -0,0 +1,1 @@
+del BuddhaBrot-MT-manual.aux BuddhaBrot-MT-manual.log BuddhaBrot-MT-manual.synctex.gz
---
+++ zip-BuddhaBrot-MT.bat
@@ -0,0 +1,2 @@
+del BuddhaBrot-MT.zip
+"C:\Program Files\7-Zip\7z.exe" a -mx9 -mfb=258 -mpass=15 BuddhaBrot-MT.zip BuddhaBrot-MT-windows-32bit.exe BuddhaBrot-MT-windows-64bit.exe BuddhaBrot-MT-manual.pdf
