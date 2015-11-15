a178196e8517afc77ded275bb2629bbf5b8ac861
Johan De Bock <johan.debock@gmail.com>
2015-11-15 13:31:26 +0100

    only keep 64bit versions

--- build-libs-windows-32bit-mingw.bat
+++
@@ -1,2 +0,0 @@
-REM http://sourceforge.net/projects/mingwbuilds/files/external-binary-packages/msys%2B7za%2Bwget%2Bsvn%2Bgit%2Bmercurial%2Bcvs-rev13.7z/download
-C:\msys\bin\sh.exe build-libs-windows-32bit-mingw.sh
